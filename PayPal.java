06f9b12373fefea247dc4bb41faed213cddf9c40
Ga-Ferrari <gabrielpferrariprof@gmail.com>
2025-11-17 16:18:00 -0300

    gramática que errei

--- PayPal.java
+++ PayPal.java
@@ -13,7 +13,7 @@ public class PayPal {
     public boolean debitar(float valorUSD) {
         if (this.saldo >= valorUSD) {
             this.saldo -= valorUSD;
-            System.out.printf("Conta do PayPal de" + this.nome + ": Débito de $" + valorUSD + " USD. Saldo atual: $" + this.saldo);
+            System.out.println("Conta do PayPal de" + this.nome + ": Debito de $" + valorUSD + " USD. Saldo atual: $" + this.saldo);
             return true;
         }
         System.out.println("PayPal: Saldo insuficiente.");
@@ -22,7 +22,7 @@ public class PayPal {
 
     public boolean creditar(float valorUSD) {
         this.saldo += valorUSD;
-        System.out.println("PayPal: Crédito de $" + valorUSD + " USD. Saldo atual: $" + this.saldo);
+        System.out.println("PayPal: Credito de $" + valorUSD + " USD. Saldo atual: $" + this.saldo);
         return true;
     }
 
